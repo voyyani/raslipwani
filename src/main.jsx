022616e262aece4fd38b8a90820f54c1ddf9ec4a
voyyani <nkarisa5388@stu.kemu.ac.ke>
2025-10-06 12:37:39 +0300

    vercel speed inights

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,18 @@
-import { SpeedInsights } from '@vercel/speed-insights/react'
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { HelmetProvider } from 'react-helmet-async';
-// Add Font Awesome
 import '@fortawesome/fontawesome-free/css/all.min.css';
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <HelmetProvider>
- 
+import { SpeedInsights } from '@vercel/speed-insights/react';
 
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <HelmetProvider>
+      <>
+        <SpeedInsights />
+        <App />
+      </>
+    </HelmetProvider>
   </React.StrictMode>
-  </HelmetProvider>
 );
