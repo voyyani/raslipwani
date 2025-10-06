39b26089c52fcc4476aea01d53104e000e3e2007
voyyani <nkarisa5388@stu.kemu.ac.ke>
2025-10-06 10:49:30 +0300

    vercel

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import App from './App';
 import { HelmetProvider } from 'react-helmet-async';
 // Add Font Awesome
 import '@fortawesome/fontawesome-free/css/all.min.css';
-
+import { SpeedInsights } from "@vercel/speed-insights/react";
 ReactDOM.createRoot(document.getElementById('root')).render(
   <HelmetProvider>
  
