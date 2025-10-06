d177723152b1cdc30807806c682000d1d994a156
voyyani <nkarisa5388@stu.kemu.ac.ke>
2025-10-06 11:03:22 +0300

    vercel

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,4 @@
+import { SpeedInsights } from '@vercel/speed-insights/react'
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
@@ -5,7 +6,6 @@ import App from './App';
 import { HelmetProvider } from 'react-helmet-async';
 // Add Font Awesome
 import '@fortawesome/fontawesome-free/css/all.min.css';
-import { SpeedInsights } from "@vercel/speed-insights/react";
 ReactDOM.createRoot(document.getElementById('root')).render(
   <HelmetProvider>
  
