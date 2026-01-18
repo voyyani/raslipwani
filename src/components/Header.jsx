795e53d9f2a4a668c174281a6a3fcc51595ed0a3
voyyani <nkarisa5388@stu.kemu.ac.ke>
2026-01-18 21:16:31 +0300

    last

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -43,7 +43,7 @@ const Header = () => {
       icon: FiTool 
     },
     { 
-      path: '/international', 
+      path: '/internationalproperties', 
       label: 'International', 
       icon: FiGrid 
     },
